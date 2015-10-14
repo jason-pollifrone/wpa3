2f2d347296ad42b5c30d445a5a06648926132af3
s3530297 <s3530297@W8079254.rmit.internal>
2015-10-14 13:43:21 +1100

    Revert "Initial Commit"
    This reverts commit 5c08c9e8dac0a04b00450b3af3cb3f91fcbee655.

--- header.php
+++
@@ -1,3 +0,0 @@
-    	<header>
-		<a href="index.php"><IMG src="images/Logo.png" alt="Header"><a/>
-    	</header>
--- navigation.php
+++
@@ -1,7 +0,0 @@
-<nav>
-	<a href="index.php"><img border="0" alt="Home" src="images/Home.png"></a>
-	<a href="sessions.php"><img border="0" alt="Sessions" src="images/Session.png"></a>
-	<a href="tickets.php"><img border="0" alt="Tickets" src="images/Ticket.png""></a>
-	<a href="contact.php"><img border="0" alt="Sessions" src="images/Contact.png"></a>
-	<a href="cart.php">Cart</a>
-</nav>
